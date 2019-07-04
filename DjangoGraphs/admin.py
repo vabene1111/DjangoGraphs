408fe009de832f0ae54705c3bd5767b8cc6cc40b
vabene1111 <vabene1234@googlemail.com>
2019-07-04 08:25:25 +0200

    styling

--- DjangoGraphs/admin.py
+++ DjangoGraphs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
-# admin.site.register(Course)
-
+admin.site.register(Graph)
+admin.site.register(GraphSelector)
+admin.site.register(Type)
+admin.site.register(Instance)
