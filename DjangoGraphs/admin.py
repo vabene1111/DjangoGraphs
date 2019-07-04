6613a51a69d5d093f566adbf4d90268434167dcc
vabene1111 <vabene1234@googlemail.com>
2019-07-04 10:32:42 +0200

    working

--- DjangoGraphs/admin.py
+++ DjangoGraphs/admin.py
@@ -6,3 +6,5 @@ admin.site.register(Graph)
 admin.site.register(GraphSelector)
 admin.site.register(Type)
 admin.site.register(Instance)
+
+admin.site.register(DataEntry)
--- DjangoGraphs/templatetags/class_tag.py
+++ DjangoGraphs/templatetags/class_tag.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='get_class')
 def get_class(value):
     return value.__class__.__name__
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
