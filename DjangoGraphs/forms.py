139b57efa7945a28b6d8fd29cdb936bcd9972c44
vabene1111 <vabene1234@googlemail.com>
2019-07-07 22:53:18 +0200

    help + backgrounds

--- DjangoGraphs/forms.py
+++ DjangoGraphs/forms.py
@@ -10,11 +10,27 @@ class GraphForm(forms.ModelForm):
         model = Graph
         fields = ('name', 'title', 'dashboard', 'public', 'selector')
 
+        help_texts = {
+            'name': _('Name of the Graph. Is displayed if no title is given.'),
+            'title': _('If not all information contained in the name should be displayed in the graph, set a title.'),
+            'dashboard': _('If true, this graph is displayed on the home page.'),
+            'public': _('If true, this graph can be seen by everyone.'),
+            'selector': _('Choose any number of selectors that should be displayed in this graph.'),
+        }
+
 
 class DisplayForm(forms.ModelForm):
     class Meta:
         model = Display
         fields = ('name', 'title', 'dashboard', 'public', 'selector')
+
+        help_texts = {
+            'name': _('Name of the Graph. Is displayed if no title is given.'),
+            'title': _('If not all information contained in the name should be displayed in the display, set a title.'),
+            'public': _('If true, this display can be seen by everyone.'),
+            'dashboard': _('If true, this display is displayed on the home page.'),
+            'selector': _('Choose the selector that is displayed by the display.'),
+        }
 
 
 class GraphSelectorForm(forms.ModelForm):
@@ -22,6 +38,13 @@ class GraphSelectorForm(forms.ModelForm):
         model = GraphSelector
         fields = ('name', 'title', 'type', 'instance', 'color')
         widgets = {'color': TextInput(attrs={'type': 'color'})}
+        help_texts = {
+            'name': _('Name of the Selector. Is displayed if no title is given.'),
+            'title': _('If not all information contained in the name should be displayed in the selector, set a title.'),
+            'type': _('Which type of data should be selected.'),
+            'instance': _('From which instance should the data be selected.'),
+            'color': _('Choose the color that this selector should use.'),
+        }
 
 
 class TypeForm(forms.ModelForm):
@@ -29,8 +52,19 @@ class TypeForm(forms.ModelForm):
         model = Type
         fields = ('name', 'description', 'unit')
 
+        help_texts = {
+            'name': _('Name of the Type.'),
+            'description': _('At the moment just for personal information about this type.'),
+            'unit': _('Unit displayed alongside data of this type.'),
+        }
+
 
 class InstanceForm(forms.ModelForm):
     class Meta:
         model = Instance
         fields = ('name', 'description')
+
+        help_texts = {
+            'name': _('Name of the Instance.'),
+            'description': _('At the moment just for personal information about this type.'),
+        }
